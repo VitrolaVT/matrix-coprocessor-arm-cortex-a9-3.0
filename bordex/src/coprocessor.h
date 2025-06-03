a92e9a14834c8b810899a42041b7445d1aa6a241
riancmd <rian.dssantos@gmail.com>
2025-06-03 11:37:57 -0300

    define funções e constantes

--- bordex/src/coprocessor.h
+++ bordex/src/coprocessor.h
@@ -4,4 +4,46 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// definição das operações load e store
+#define storeMatrixA 0b0111
+#define storeMatrixB 0b1000
+#define loadMatrixResult 0b1001
+
+// definição dos tamanhos
+#define M2 0b00
+#define M3 0b01
+#define M4 0b10
+#define M5 0b11
+
+// definição das operações
+#define SOM 0b000
+#define SUB 0b001
+#define MULM 0b010
+#define MULI 0b011
+#define DET 0b100
+#define TRANS 0b101
+#define OPP 0b110
+
+// protótipo das funções de interface
+void start_program(void);
+
+int operate_buffer_send(int opcode, int size, int position, int8_t* matriz);
+
+int calculate_matriz(int opcode, int size, int position);
+
+int operate_buffer_receive(int opcode, int size, int position, int8_t* matriz);
+
+int signal_overflow(void);
+
+void exit_program(void);
+
+// protótipo das funções do coprocessador
+int8_t soma(int8_t* matrizA, int8_t* matrizB, int8_t* res);
+int8_t subt(int8_t* matrizA, int8_t* matrizB, int8_t* res);
+int8_t multm(int8_t* matrizA, int8_t* matrizB, int8_t* res);
+int8_t multi(int8_t* matrizA, int8_t* matrizB, int8_t* res);
+int8_t detm(int8_t* matrizA, int size, int8_t* res);
+int8_t transm(int8_t* matrizA, int8_t* res);
+int8_t oppm(int8_t* matrizA, int8_t* res);
+
 #endif
