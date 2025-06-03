876e9735eff72e8228fc6ea2d524b0670ea88756
riancmd <rian.dssantos@gmail.com>
2025-06-03 12:18:13 -0300

    ✨ feat: convolução

--- bordex/src/coprocessor.c
+++ bordex/src/coprocessor.c
@@ -40,3 +40,51 @@ void transm(int8_t* matrizA, int8_t* res){
 void oppm(int8_t* matrizA, int8_t* res){
 
 }
+
+// função que realiza a operação de convolução e retorna o elemento correspondente
+void conv(int8_t* matrizA, int8_t* filtroX, int8_t* filtroY, uint8_t* res){
+    // definição de variáveis
+    int soma_X, soma_Y, i, elemento;
+    int8_t tempMX[25];
+    int8_t tempMY[25];
+
+    // inicializa res com 0s
+    memset(res, 0, 25 * sizeof(int8_t));
+
+    // realiza produto e soma
+    multConv(matrizA, filtroX, tempMX);
+    multConv(matrizA, filtroY, tempMY);
+
+    soma_X = soma_Y = 0;
+    
+    for(i = 0; i < 25; i++){
+        soma_X += tempMX[i];
+        soma_Y += tempMY[i];
+    }
+
+    // realiza operação de módulo
+    elemento = round(sqrt(pow(soma_X, 2) + pow(soma_Y, 2)));
+
+    // se maior que 255, satura
+    if (elemento > 255){
+        elemento = 255;
+    }
+
+    // casta o valor pra unsigned (nn importa ser signed)
+    (*res) = (uint8_t)elemento;
+}
+
+int8_t multConv(int8_t* matrizA, int8_t* matrizB, int8_t* res){
+    int i;
+    // multiplica cada elemento pelo elemento da matrizB correspondente (filtro) com saturação
+    for (i=0; i<25; i++){
+        res[i] = (matrizA[i] * matrizB[i]);
+
+        if (res[i] > 127) {
+            res[i] = (int8_t)(127);            
+        }
+        else if (res[i] < - 128){
+            res[i] = (int8_t)(-128);
+        }
+    }
+}
--- bordex/src/coprocessor.h
+++ bordex/src/coprocessor.h
@@ -48,4 +48,7 @@ void transm(int8_t* matrizA, int8_t* res);
 void oppm(int8_t* matrizA, int8_t* res);
 void conv(int8_t* matrizA, int8_t* matrizB, int8_t* res);
 
+// operações intermediárias
+int8_t multConv(int8_t* matrizA, int8_t* matrizB, int8_t* res);
+
 #endif
